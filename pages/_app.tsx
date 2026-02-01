ceafa1e34d94902a90516ce8a2fb8df675e7c320
kukusha <yeabsira0514@gmail.com>
2026-02-01 11:37:59 +0300

    feat: add Next.js `_app.tsx` for global layout, theme provisioning, and footer integration.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '../lib/ThemeContext'
 import '../styles/globals.css'
+import Footer from '../components/Footer'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
+      <Footer />
     </ThemeProvider>
   )
 }
