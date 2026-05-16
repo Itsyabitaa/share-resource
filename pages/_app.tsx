a432c9a7968b31fe3e784dd631857b5a107c927f
kukusha <yeabsira0514@gmail.com>
2026-05-16 21:24:25 +0300

    feat: implement main application entry point and homepage editor interface with file upload and storage management

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,17 @@ import '../styles/globals.css'
 import Footer from '../components/Footer'
 
 import AppLayout from '../components/AppLayout'
+import { SidebarProvider } from '../lib/SidebarContext'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
-      <AppLayout>
-        <Component {...pageProps} />
-        <Footer />
-      </AppLayout>
+      <SidebarProvider>
+        <AppLayout>
+          <Component {...pageProps} />
+          <Footer />
+        </AppLayout>
+      </SidebarProvider>
     </ThemeProvider>
   )
 }
